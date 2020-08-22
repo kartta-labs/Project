7a5ffa6bd8ea1b13baeeb95f10c83cf4e8e2f35d
Mark Phillips <embeepea@google.com>
2020-08-21 20:30:55 -0400

    add map home page app, shared logo & app menu

--- bootstrap.sh
+++ bootstrap.sh
@@ -78,4 +78,12 @@ fi
 LOG_INFO "Building Reservoir image."
 sudo ./dcwrapper -f ./h3dmr/docker-compose.yml build h3dmr
 
+if [ "${MAP_REPO}" != "" ] ; then
+  git clone ${MAP_REPO} map
+  # map repo needs a copy of antqiue repo under it.  There are definitely better ways to do this
+  # (git submodule?), but for now we just clone it manually:
+  (cd map ; git clone ${ANTIQUE_REPO} antique)
+  sudo ./dcwrapper build map
+fi
+
 ) 2>&1 | tee bootstrap.log
